cf25ed6e278cdbcefd55be42fdb008a952fbfda1
Tokhirbek <tokhirjonorifjonov2203@gmail.com>
2023-09-18 20:17:52 +0500

    first commit

---
+++ src/main/java/com/vention/stock_market_share/StockMarketShareApplication.java
@@ -0,0 +1,13 @@
+package com.vention.stock_market_share;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMarketShareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockMarketShareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vention/stock_market_share/StockMarketShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vention.stock_market_share;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMarketShareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
